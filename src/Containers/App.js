86d6ad806fe62cb2a90cdbd609b50f3e167dfd61
Ivan Ha <hangolam@gmail.com>
2017-11-28 17:57:23 +0800

    use render-props

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,42 +1,44 @@
 import React, { PureComponent } from 'react'
 import styles from './styles'
 
-const withMouse = WrappedComponent =>
-  class extends PureComponent {
-    state = {
-      x: 0,
-      y: 0
-    }
-
-    handleMouseMove = ({ clientX, clientY }) => {
-      this.setState({
-        x: clientX,
-        y: clientY
-      })
-    }
-
-    render () {
-      return (
-        <div onMouseMove={this.handleMouseMove}>
-          <WrappedComponent mouse={this.state} {...this.props} />
-        </div>
-      )
-    }
+class Mouse extends PureComponent {
+  state = {
+    x: 0,
+    y: 0
   }
 
-class App extends PureComponent {
-  render () {
-    const { message, mouse } = this.props
-    const { x, y } = mouse
+  handleMouseMove = ({ clientX, clientY }) => {
+    this.setState({
+      x: clientX,
+      y: clientY
+    })
+  }
 
+  render () {
     return (
-      <div style={styles.container}>
-        <h1>
-          The mouse position is ({x}, {y}). The message is {message}
-        </h1>
+      <div onMouseMove={this.handleMouseMove}>
+        {this.props.children(this.state)}
       </div>
     )
   }
 }
 
-export default withMouse(App)
+class App extends PureComponent {
+  render () {
+    const { message } = this.props
+
+    return (
+      <Mouse>
+        {({ x, y }) => (
+          <div style={styles.container}>
+            <h1>
+              The mouse position is ({x}, {y}). The message is {message}
+            </h1>
+          </div>
+        )}
+      </Mouse>
+    )
+  }
+}
+
+export default App
